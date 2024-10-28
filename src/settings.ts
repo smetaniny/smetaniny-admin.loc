cfd267c9d34292efb02150fd9763bded8f8e95b1
Smetaniny <sm.sergey.v@yandex.ru>
2024-10-28 07:50:17 +0300

    ReactAdminRouting

--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,7 @@
-// src/settings.ts
-export const CHANGE_THEME = "CHANGE_THEME";
-export const URL_API = "http://http://prismasm.loc/admin";
-export const AUTH_URL_API = "http://prismasm.loc/admin/login";
-export const TOKEN_URL_API = "http://prismasm.loc/admin/autha_verification";
+export const URL_API = `/admin`;
+export const AUTH_URL_API = `/admin/login`;
 
-// Если нужно, можно экспортировать все константы в одном объекте
 export default {
-  CHANGE_THEME,
-  URL_API,
-  AUTH_URL_API,
-  TOKEN_URL_API,
+    URL_API,
+    AUTH_URL_API,
 };
